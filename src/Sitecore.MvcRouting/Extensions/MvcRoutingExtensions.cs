496c85ca361b36d287a9c1722a13fde5211e78d8
Stephen Pope <stp@sitecore.net>
2011-11-11 12:31:39 +0000

    Moved SitecoreItemId into a separate parameter

--- src/Sitecore.MvcRouting/Extensions/MvcRoutingExtensions.cs
+++ src/Sitecore.MvcRouting/Extensions/MvcRoutingExtensions.cs
@@ -11,9 +11,9 @@ namespace Sitecore.MvcRouting.Extensions
         /// <param name="routes">The route collection.</param>
         /// <param name="routeName">The name of the route.</param>
         /// <param name="routeUrl">The URL pattern for the route.</param>
-        public static Route MapSitecoreRoute(this RouteCollection routes, string routeName, string routeUrl)
+        public static Route MapSitecoreRoute(this RouteCollection routes, string routeName, string sitecoreItemId, string routeUrl)
         {
-            return routes.MapSitecoreRoute(routeName, routeUrl, null, null, null);
+            return routes.MapSitecoreRoute(routeName, sitecoreItemId, routeUrl, null, null, null);
         }
 
         /// <summary>Provides a way to define routes for Sitecore applications.</summary>
@@ -22,9 +22,9 @@ namespace Sitecore.MvcRouting.Extensions
         /// <param name="routeName">The name of the route.</param>
         /// <param name="routeUrl">The URL pattern for the route.</param>
         /// <param name="defaults">Default values for the route parameters.</param>
-        public static Route MapSitecoreRoute(this RouteCollection routes, string routeName, string routeUrl, object defaults)
+        public static Route MapSitecoreRoute(this RouteCollection routes, string routeName, string sitecoreItemId, string routeUrl, object defaults)
         {
-            return routes.MapSitecoreRoute(routeName, routeUrl, defaults, null, null);
+            return routes.MapSitecoreRoute(routeName, sitecoreItemId, routeUrl, defaults, null, null);
         }
 
         /// <summary>Provides a way to define routes for Sitecore applications.</summary>
@@ -34,9 +34,9 @@ namespace Sitecore.MvcRouting.Extensions
         /// <param name="routeUrl">The URL pattern for the route.</param>
         /// <param name="defaults">Default values for the route parameters.</param>
         /// <param name="constraints">Constraints that a URL request must meet in order to be processed as this route.</param>
-        public static Route MapSitecoreRoute(this RouteCollection routes, string routeName, string routeUrl, object defaults, object constraints)
+        public static Route MapSitecoreRoute(this RouteCollection routes, string routeName, string sitecoreItemId, string routeUrl, object defaults, object constraints)
         {
-            return routes.MapSitecoreRoute(routeName, routeUrl, defaults, constraints, null);
+            return routes.MapSitecoreRoute(routeName, sitecoreItemId, routeUrl, defaults, constraints, null);
         }
 
         /// <summary>Provides a way to define routes for Sitecore applications.</summary>
@@ -48,35 +48,19 @@ namespace Sitecore.MvcRouting.Extensions
         /// <param name="constraints">Constraints that a URL request must meet in order to be processed as this route.</param>
         /// <param name="dataTokens">Values that are associated with the route that are not used to determine whether a route matches a URL pattern.</param>
         /// <exception cref="T:System.ArgumentNullException">The <paramref name="routeUrl" /> parameter is null.</exception>
-        public static Route MapSitecoreRoute(this RouteCollection routes, string routeName, string routeUrl, object defaults, object constraints, object dataTokens)
+        public static Route MapSitecoreRoute(this RouteCollection routes, string routeName, string sitecoreItemId, string routeUrl, object defaults, object constraints, object dataTokens)
         {
-            var sitecoreItemId = string.Empty;
-
             if (routeUrl == null)
             {
                 throw new ArgumentNullException("routeUrl");
             }
 
-            if (routeUrl.StartsWith("{"))
+            if(sitecoreItemId == null)
             {
-                Guid guid;
-
-                var parts = routeUrl.Split('/');
-
-                if (parts.Length == 0)
-                {
-                    throw new ArgumentException("RouteUrl must start with a Sitecore GUID", "routeUrl");
-                }
-
-                if (Guid.TryParse(parts[0], out guid))
-                {
-                    sitecoreItemId = guid.ToString();
-                }
+                throw new ArgumentNullException("sitecoreItemId");
             }
-            else
-            {
-                throw new ArgumentException("RouteUrl must start with a Sitecore GUID", "routeUrl");
-            }
+
+            routeUrl = string.Format("{0}/{1}", sitecoreItemId, routeUrl);
 
             var route = new Route(routeUrl, new RouteValueDictionary(defaults), new RouteValueDictionary(constraints), new RouteValueDictionary(dataTokens), new MvcRoutingRouteHandler(sitecoreItemId));
             route.Defaults.Add("routeName", routeName);
